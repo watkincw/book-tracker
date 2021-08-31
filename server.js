1761c579ea61b4e78410a9c1137a203cf3468938
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-08-31 12:56:50 -0500

    working on setting up the server; not working yet

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection.js');
-const apiRoutes = require('./controllers/apiRoutes');
-const htmlRoutes = require('./controllers/htmlRoutes');
+// const apiRoutes = require('./controllers/apiRoutes');
+// const htmlRoutes = require('./controllers/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
