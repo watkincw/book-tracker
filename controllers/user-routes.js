2255d4aea7def5bdfdbcb90ed84956c05b5706e9
Bri <bhilliker@gmail.com>
2021-08-31 09:21:51 -0500

    begin user routes and model

---
+++ controllers/user-routes.js
@@ -0,0 +1,11 @@
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  
+    // pass in req.body instead to only update what's passed through
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+})
