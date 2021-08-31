c671e494c687ec4c68d8492393052bf1131a2a67
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-08-31 11:19:24 -0500

    placeholder files for './utils' folder

---
+++ utils/helpers.js
@@ -0,0 +1,5 @@
+// Currently just a placeholder file for the file structure so the 
+// './utils' folder shows up on github and will be pulled when 
+// we run 'git pull origin develop'
+
+// If we end up not needing this file, can just delete
