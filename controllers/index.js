3892ea5f8e2416696446a3aba8b6e3f8ddcc6de0
Bri <bhilliker@gmail.com>
2021-09-01 17:49:41 -0500

    add home-routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
 const userRoutes = require('./user-routes.js');
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
