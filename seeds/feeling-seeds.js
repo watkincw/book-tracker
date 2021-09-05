61d039fca453a6af6f7d635ec529963e48f10f73
Bri <bhilliker@gmail.com>
2021-09-05 12:34:01 -0500

    update seed files to seed database without errors

--- seeds/feeling-seeds.js
+++ seeds/feeling-seeds.js
@@ -27,6 +27,31 @@ const feelingData = [
     book_id: 9780142005255
   },
   {
+    type: 'wish',
+    user_id: 6,
+    book_id: 9780688084882
+  },
+  {
+    type: 'wish',
+    user_id: 6,
+    book_id: 9781931195126
+  },
+  {
+    type: 'wish',
+    user_id: 6,
+    book_id: 0143105426
+  },
+  {
+    type: 'wish',
+    user_id: 6,
+    book_id: 9780689835605
+  },
+  {
+    type: 'wish',
+    user_id: 6,
+    book_id: 9780142005255
+  },
+  {
     type: 'like',
     user_id: 2,
     book_id: 9780688084882
@@ -43,18 +68,8 @@ const feelingData = [
   },
   {
     type: 'wish',
-    user_id: 5,
-    book_id: 9780689835605
-  },
-  {
-    type: 'wish',
     user_id: 3,
     book_id: 9780142005255
-  },
-  {
-    type: 'like',
-    user_id: 6,
-    book_id: 9780688084882
   },
   {
     type: 'dislike',
@@ -63,42 +78,8 @@ const feelingData = [
   },
   {
     type: 'wish',
-    user_id: 3,
-    book_id: 0143105426
-  },
-  {
-    type: 'wish',
-    user_id: 2,
+    user_id: 5,
     book_id: 9780689835605
-  },
-  {
-    type: 'wish',
-    user_id: 2,
-    book_id: 9780142005255
-  },{
-    type: 'wish',
-    user_id: 6,
-    book_id: 9780688084882
-  },
-  {
-    type: 'wish',
-    user_id: 6,
-    book_id: 9781931195126
-  },
-  {
-    type: 'wish',
-    user_id: 6,
-    book_id: 0143105426
-  },
-  {
-    type: 'wish',
-    user_id: 6,
-    book_id: 9780689835605
-  },
-  {
-    type: 'wish',
-    user_id: 6,
-    book_id: 9780142005255
   }
 ];
 
--- seeds/index.js
+++ seeds/index.js
@@ -5,17 +5,18 @@ const seedUser = require('./user-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true }); // drops database and re-schemas
   console.log('\n----- DATABASE SYNCED -----\n');
   
-  await seedFeeling();
-  console.log('\n----- FEELINGS SEEDED -----\n');
-
-  await seedBooks();
-  console.log('\n----- BOOKS SEEDED -----\n');
-
+  // user data doesn't rely on any constraints so we put it first
   await seedUser();
   console.log('\n----- USERS SEEDED -----\n');
+  
+  await seedBooks();
+  console.log('\n----- BOOKS SEEDED -----\n');
+  // feeling data has book id constraint--so we do books before feelings :)
+  await seedFeeling();
+  console.log('\n----- FEELINGS SEEDED -----\n');
 
   process.exit(0);
 };
