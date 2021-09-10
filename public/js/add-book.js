f5ba1fe6be4d4c59c5577788ff696d3878bdf466
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-10 13:04:55 -0500

    indentation changes

--- public/js/add-book.js
+++ public/js/add-book.js
@@ -1,32 +1,32 @@
 async function newFormHandler(event) {
-  event.preventDefault();
-  console.log('You clicked a button.');
+	event.preventDefault();
+	console.log('You clicked a button.');
 
-  const title = document.querySelector('input[name="book-title"]').value;
-  const isbn = document.querySelector('input[name="book-isbn"]').value;
-  const author = document.querySelector('input[name="book-author"]').value;
-  const feeling = document.querySelector('input[name="book-feeling"]:checked').value;
-  const review = document.querySelector('input[name="book-review"]').value;
+	const title = document.querySelector('input[name="book-title"]').value;
+	const isbn = document.querySelector('input[name="book-isbn"]').value;
+	const author = document.querySelector('input[name="book-author"]').value;
+	const feeling = document.querySelector('input[name="book-feeling"]:checked').value;
+	const review = document.querySelector('input[name="book-review"]').value;
 
-  const response = await fetch('/dashboard', {
-    method: 'POST',
-    body: JSON.stringify({
-      title,
-      isbn,
-      author,
-      feeling,
-      review
-    }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+	const response = await fetch('/dashboard', {
+		method: 'POST',
+		body: JSON.stringify({
+			title,
+			isbn,
+			author,
+			feeling,
+			review
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 
-  if (response.ok) {
-    document.location.replace('/dashboard');
-  } else {
-    alert(response.statusText);
-  }
+	if (response.ok) {
+		document.location.replace('/dashboard');
+	} else {
+		alert(response.statusText);
+	}
 }
 
 document.querySelector('.new-book-form').addEventListener('submit', newFormHandler);
