e67c61aaa861f1ef6ebc8af3f98683378ea385c3
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-10 09:58:36 -0500

    indentation

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,47 +5,47 @@ const withAuth = require('../utils/auth');
 //localhost:3001/dashboard subaddress
 
 router.post('/', withAuth, (req, res) => {
-  Book.create({
-    isbn: req.body.isbn,
-    title: req.body.title,
-    author: req.body.author,
-  })
-  Feeling.create({
-    // id: Automatically created using autoIncrement
-    type: req.body.feeling,
-    user_id: req.session.user_id,
-    book_isbn: req.body.isbn, 
-    review: req.body.review
-  })
-    .then(dbBookData => res.json(dbBookData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+	Book.create({
+		isbn: req.body.isbn,
+		title: req.body.title,
+		author: req.body.author,
+	})
+	Feeling.create({
+		// id: Automatically created using autoIncrement
+		type: req.body.feeling,
+		user_id: req.session.user_id,
+		book_isbn: req.body.isbn, 
+		review: req.body.review
+	})
+		.then(dbBookData => res.json(dbBookData))
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 router.get('/', withAuth, (req, res) => {
-  Book.findAll({
-    include: {
-      model: Feeling,
-      where: {
-        user_id: req.session.user_id
-      }
-    }
-  })
-    .then(dbBookData => {
-      // serialize data before passing to template
-      const books = dbBookData.map(book => book.get({ plain: true }));
-      // console.log(dbBookData);
-      const likes = books.filter(book => book.feelings[0].type === 'like');
-      const dislikes = books.filter(book => book.feelings[0].type === 'dislike');
-      const wishes = books.filter(book => book.feelings[0].type === 'wish');
-      res.render('dashboard', { books, likes, dislikes, wishes, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+	Book.findAll({
+		include: {
+			model: Feeling,
+			where: {
+				user_id: req.session.user_id
+			}
+		}
+	})
+		.then(dbBookData => {
+			// serialize data before passing to template
+			const books = dbBookData.map(book => book.get({ plain: true }));
+			// console.log(dbBookData);
+			const likes = books.filter(book => book.feelings[0].type === 'like');
+			const dislikes = books.filter(book => book.feelings[0].type === 'dislike');
+			const wishes = books.filter(book => book.feelings[0].type === 'wish');
+			res.render('dashboard', { books, likes, dislikes, wishes, loggedIn: true });
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
